a20a2849316d120d4ca3d180249f0d12c57d4030
aarsh <68937277+LuC-9@users.noreply.github.com>
2025-02-09 03:45:14 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,7 @@ export default {
         sm: "calc(var(--radius) - 4px)",
       },
       colors: {
+        darkRed: '#8B0000'
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
         card: {
